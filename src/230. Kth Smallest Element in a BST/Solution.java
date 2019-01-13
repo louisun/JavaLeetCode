67e17c03aff14e1d2dd22b94e6f6d98e683819d8
louisun <read_on@126.com>
2019-01-13 23:47:47 +0800

    :rocket: daily practice

--- src/230. Kth Smallest Element in a BST/Solution.java
+++ src/230. Kth Smallest Element in a BST/Solution.java
@@ -1,1 +1,37 @@
-// 日期: 2019-01-13 20:33:34
+// 日期: 2019-01-13 23:17:52
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private int cnt = 0;
+    private int e;
+    public int kthSmallest(TreeNode root, int k) {
+        inOrder(root, k);
+        return e;
+    }
+    
+    public void inOrder(TreeNode root, int k){
+        if(root==null) return;
+        if(root.left != null){
+            inOrder(root.left, k);
+        }
+        
+        cnt++;
+        if(cnt == k){
+            e = root.val;
+            return;
+        }
+        
+        if(root.right != null){
+            inOrder(root.right, k);
+        }
+    }
+}
--- src/235. Lowest Common Ancestor of a Binary Search Tree/Solution.java
+++ src/235. Lowest Common Ancestor of a Binary Search Tree/Solution.java
@@ -1,1 +1,42 @@
-// 日期: 2019-01-13 20:41:52
+// 日期: 2019-01-13 23:17:52
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private TreeNode t = null;
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(p.val > q.val){
+            foo(root, q, p);
+        }
+        else{
+            foo(root, p, q);            
+        }
+        return t;
+
+    }
+    
+    public void foo(TreeNode root, TreeNode p, TreeNode q){
+        if(root == null) return;
+        
+        if(root.val >= p.val && root.val <= q.val){
+            t = root;
+            return;
+        }
+        else if(root.val < p.val){
+            lowestCommonAncestor(root.right, p, q);
+
+        }
+        else if(root.val > q.val){
+            lowestCommonAncestor(root.left, p, q);            
+        }   
+    }
+}
+
